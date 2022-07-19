bd487c918321af422eae705314bd688f9db8b51d
Kupreenko Oleg <89996212885@mail.ru>
2022-07-19 14:13:30 +0300

    Completed Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,4 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int a = int.Parse(Console.ReadLine());
+int b = (int)Math.Log10(a)-2;
+Console.WriteLine(b < 0 ? "Третьей цифры нет" : (a % (int)Math.Pow(10, b + 1) / (int)Math.Pow(10, b)).ToString());
